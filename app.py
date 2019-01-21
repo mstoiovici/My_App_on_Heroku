9aab3a0d5e99c5b4addbe6b63d76f76abb969900
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2019-01-21 21:03:46 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 21 20:55:55 2019
+
+@author: maria
+"""
+
+from flask import Flask,render_template,request
+app=Flask("MyApp")
+
+@app.route("/")
+def hello():
+    return "Hello world!!!!!"
+
+@app.route("/Mariana")
+def hello_someone():
+    return "This will be Mariana's web app"
+
+@app.route("/Mariana/<name>")     ##### how do I return the else statement in my html, cause if I don't give a name, I'll get a not found URL error
+def hello_someone_else(name):
+    return render_template("hello.html", name=name.title())
+######Get information from user
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data=request.form
+    print(form_data["email"])
+    return "All OK"
+
+
+app.run(debug=True)
